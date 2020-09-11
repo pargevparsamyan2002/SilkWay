4d771823f6c61f77d7257ba6d1749c9bd3099a79
pargevparsamyan2002 <44526575+pargevparsamyan2002@users.noreply.github.com>
2020-09-11 15:56:38 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+
+namespace NewSilkWay
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+        // using main classes
+        public static wwwroot.Root Root = new wwwroot.Root();
+        // Models
+        public static wwwroot.Models.StaticFiles StaticFiles = new wwwroot.Models.StaticFiles();
+
+        // using main variables
+        public static string JWTKey = "kghskcksvwrlwuydugvaadguytfuysvuv";
+        public static string URL = "http://192.168.1.202:11111";/* "http://localhost:11111";*/
+
+
+
+        //StaticFiles
+       
+        public static XmlDocument SqlConnectionStrings = StaticFiles.xmlDocumentSqlConnections();// SQL Connections
+        public static XmlDocument XmlMail = StaticFiles._xmlDocumentMail();// Mail rekvizites
+
+
+
+        //Tests
+
+        public static wwwroot.Tests.Tests Tests = new wwwroot.Tests.Tests();
+
+        
+
+
+
+
+
+
+
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+            .UseUrls(URL)
+                .UseStartup<Startup>();
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System.IO;
+
+namespace NewSilkWay
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+           
+            app.Map("/themes", themes);
+            app.Map("/api", api);
+            app.Run(async (context) =>
+            {
+                wwwroot.Controls.PageControls PageControls = new wwwroot.Controls.PageControls();
+                  await context.Response.WriteAsync(PageControls.HomePage(context));
+            });
+        }
+        
+        private void themes(IApplicationBuilder app)
+        {
+            wwwroot.Controls.PageControls PageControls = new wwwroot.Controls.PageControls();
+            app.Run(async (context) =>
+            {
+                PageControls.Themes(context);
+                if (PageControls._ResponseType == "text")
+                {
+                    await context.Response.WriteAsync(PageControls._ResponseText);
+                }
+                else if (PageControls._ResponseType == "path")
+                {
+                    await context.Response.SendFileAsync(PageControls._ResponseFilePath);
+                }
+            });
+        }
+        
+        private void api(IApplicationBuilder app)
+        {
+            
+            wwwroot.Controls.ControlApi ControlApi = new wwwroot.Controls.ControlApi();
+            app.Run(async (context) =>
+            {
+                (string,string) Response = ControlApi.Response(context);
+                string _ResponseType = Response.Item1;
+                if (_ResponseType == "SendFile")
+                {
+                    await context.Response.SendFileAsync(Response.Item2);
+                }
+                else if (_ResponseType  == "WriteText")
+                {
+                    await context.Response.WriteAsync(Response.Item2);
+                }
+                else if(_ResponseType == "empty")
+                {
+
+                }
+            });
+        }
+
+    }
+}
